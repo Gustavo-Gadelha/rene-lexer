3c1bea26938eb3df3d3b34c1787b237b23cc7421
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-18 10:47:07 -0300

    chore: formata as classes PalavraChave e Simbolos

--- src/edu/fafic/auxiliares/PalavrasChave.java
+++ src/edu/fafic/auxiliares/PalavrasChave.java
@@ -15,80 +15,37 @@ public class PalavrasChave {
     private static final String VOID = "void";
     private static final String WHILE = "while";
 
-    public static boolean isPalavraChave(StringBuffer palavra) {
-
-        String palavra1 = palavra.toString();
-
-        if (palavra1.equals(CHAR) || palavra1.equals(ELSE) || palavra1.equals(FALSE) || palavra1.equals(INT) || palavra1.equals(IF)
-            || palavra1.equals(MAIN) || palavra1.equals(OUT) || palavra1.equals(PRINTF) || palavra1.equals(RETURN)
-            || palavra1.equals(STATIC) || palavra1.equals(VOID) || palavra1.equals(WHILE)) {
-
-            return true;
-
-        }
-
-        return false;
-
+    public static boolean isPalavraChave(StringBuffer stringBuffer) {
+        String palavra = stringBuffer.toString();
+        return palavra.equals(CHAR)
+               || palavra.equals(ELSE)
+               || palavra.equals(FALSE)
+               || palavra.equals(INT)
+               || palavra.equals(IF)
+               || palavra.equals(MAIN)
+               || palavra.equals(OUT)
+               || palavra.equals(PRINTF)
+               || palavra.equals(RETURN)
+               || palavra.equals(STATIC)
+               || palavra.equals(VOID)
+               || palavra.equals(WHILE);
     }
 
     public static int tipoPalavraChave(StringBuffer palavra) {
-
-        String palavra1 = palavra.toString();
-
-        int resultado;
-
-        if (palavra1.equals(CHAR)) {
-
-            resultado = Token.CHAR;
-
-        } else if (palavra1.equals(ELSE)) {
-
-            resultado = Token.ELSE;
-
-        } else if (palavra1.equals(FALSE)) {
-
-            resultado = Token.FALSE;
-
-        } else if (palavra1.equals(INT)) {
-
-            resultado = Token.INT;
-
-        } else if (palavra1.equals(IF)) {
-
-            resultado = Token.IF;
-
-        } else if (palavra1.equals(MAIN)) {
-
-            resultado = Token.MAIN;
-
-        } else if (palavra1.equals(OUT)) {
-
-            resultado = Token.OUT;
-
-        } else if (palavra1.equals(PRINTF)) {
-
-            resultado = Token.PRINTF;
-
-        } else if (palavra1.equals(RETURN)) {
-
-            resultado = Token.RETURN;
-
-        } else if (palavra1.equals(STATIC)) {
-
-            resultado = Token.STATIC;
-
-        } else if (palavra1.equals(VOID)) {
-
-            resultado = Token.VOID;
-
-        } else {
-
-            resultado = Token.WHILE;
-
-        }
-
-        return resultado;
-
+        return switch (palavra.toString()) {
+            case CHAR -> Token.CHAR;
+            case ELSE -> Token.ELSE;
+            case FALSE -> Token.FALSE;
+            case INT -> Token.INT;
+            case IF -> Token.IF;
+            case MAIN -> Token.MAIN;
+            case OUT -> Token.OUT;
+            case PRINTF -> Token.PRINTF;
+            case RETURN -> Token.RETURN;
+            case STATIC -> Token.STATIC;
+            case VOID -> Token.VOID;
+            default -> Token.WHILE;
+            // TODO: O default deveria levantar um erro caso não reconheça a palavra chave, ou retorna -1
+        };
     }
-
 }
--- src/edu/fafic/auxiliares/Simbolos.java
+++ src/edu/fafic/auxiliares/Simbolos.java
@@ -3,15 +3,10 @@ package edu.fafic.auxiliares;
 public class Simbolos {
 
     public static boolean isLetra(int caractere) {
-
         return Character.isLetter(caractere) && caractere != '_';
-
     }
 
     public static boolean isLetraOuDigito(int caractere) {
-
         return Character.isLetterOrDigit(caractere) && caractere != '_';
-
     }
-
 }
